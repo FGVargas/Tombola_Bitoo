3d9381f06575579e86b3c34e35723fd9f2183804
AdanUATx <20191445@uatx.mx>
2022-10-05 11:36:00 -0500

    REGISTRO ARTICULOS

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {ServiceService} from "../../Services/service.service";
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  encapsulation:ViewEncapsulation.None,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   listaEmpleado:Array <any>=[];
-  constructor(private servicio:ServiceService) { }
+  constructor(private servicio:ServiceService, private home:NgbModal) { }
 
   ngOnInit(): void {
     this.obtener();
@@ -17,5 +19,9 @@ export class HomeComponent implements OnInit {
   obtener(){
     this.servicio.listaPersonas().subscribe(result=>{this.listaEmpleado=result.data.usuario;},
       error=>{console.log(error)});
+  }
+
+  openSM(agregarArticulos: any){
+    this.home.open(agregarArticulos,{size:'sm'});
   }
 }
